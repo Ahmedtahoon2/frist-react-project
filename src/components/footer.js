cf687564b9ee0a964298a2e7847482bf29a66ddb
Ahmedtahoon2 <tahooooon3000@gmail.com>
2023-12-19 13:10:36 +0200

    Fix: create components

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                Designed and developed by Ahmed Tahoon
+                <span>🧡</span>
+            </footer>
+        </>
+    );
+};
+export default Footer;
